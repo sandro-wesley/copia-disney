187dc57746373d61e95419b2f30c1ef38b05873c
Wesley <sandro.wesley521@gmail.com>
2022-01-16 20:39:33 -0300

    Prototipo da primeira parte

---
+++ script.js
@@ -0,0 +1,8 @@
+var header = document.getElementById('header')
+function fixar() {
+   
+    header.style.position = "fixed"
+    header.style.width = "96vw"
+    header.style.marginTop = "0px"
+
+}
