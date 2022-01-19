17cc8a923ddde7679d64648582abf4bdad8a3c3e
Wesley <sandro.wesley521@gmail.com>
2022-01-19 17:41:33 -0300

    versao web finalizado

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 var header = document.getElementById('header')
+var logo = document.getElementById('logo-header')
+var btnAssinar = document.getElementById('btn-assine')
+
+
+var main =   document.getElementById('main')
+
 function fixar() {
    
-    header.style.position = "fixed"
-    header.style.width = "96vw"
-    header.style.marginTop = "0px"
+    header.style.position = "fixed";
+    header.style.width = "96vw";
+    
+    logo.style.visibility = "visible"
+
+    btnAssinar.style.visibility = "visible"
+
+    main.style.marginTop = "0px"
 
 }
